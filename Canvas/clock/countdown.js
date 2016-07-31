1da56af324751fd9eb51f780ecb8bbb65cfeb9ac
zhangpeng <2301328966@qq.com>
2016-07-31 16:59:56 +0800

    canvas physical motion

--- Canvas/clock/countdown.js
+++ Canvas/clock/countdown.js
@@ -6,81 +6,195 @@ var WINDOW_HEIGHT = 768;
 var RADIUS = 8;
 var MARGIN_TOP = 30;
 var MARGIN_LEFT = 60;
-const endTime = new Date(2016,7,1,0,0,0);
+const endTime = new Date(2016, 7, 1, 0, 0, 0);
 var curShowTimeSeconds = 0;
 
-window.onload = function(){
+var balls = [];
+const colors = ["#33B5E5", "#0099CC", "#AA66CC", "#9933CC", "#99CC00", "#669900", "#FFBB33", "#FF8800", "#FF4444", "#CC0000"];
+
+window.onload = function () {
+    WINDOW_WIDTH = document.body.clientWidth;
+    WINDOW_HEIGHT = document.body.clientHeight;
+
+    MARGIN_LEFT = Math.round(WINDOW_WIDTH / 10);
+    RADIUS = Math.round(WINDOW_WIDTH * 4 / 5 / 108) - 1;
+    MARGIN_TOP = Math.round(WINDOW_HEIGHT / 5);
 
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
 
     canvas.width = WINDOW_WIDTH;
     canvas.height = WINDOW_HEIGHT;
+
     curShowTimeSeconds = getCurShowTimeSeconds();
-    setInterval(function(){
+    setInterval(function () {
         render(context);
         updateTime();
-    },50);
+    }, 50);
 
-}
-
-function updateTime(){
-    var nextShowTimeSeconds = getCurShowTimeSeconds();
-    // var nextHours = parseInt(nextShowTimeSeconds/3600);
-    // var nextMinutes = parseInt((nextShowTimeSeconds - nextHours*3600)/60);
-    // var nextSeconds = nextShowTimeSeconds % 60;
-    //
-    // var curHours = parseInt(curShowTimeSeconds/3600);
-    // var curMinutes = parseInt((curShowTimeSeconds - curHours*3600)/60);
-    // var curSeconds = curShowTimeSeconds % 60;
-
-    if(nextShowTimeSeconds != curShowTimeSeconds){
-        curShowTimeSeconds = nextShowTimeSeconds;
-    }
-}
-function getCurShowTimeSeconds(){
-    var curTime = new Date();
-    var ret = endTime.getTime() - curTime.getTime();
-    ret = Math.round(ret/1000);
-
-    return ret >=0 ? ret : 0;
-}
-
-function render(cxt){
-
-    cxt.clearRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
-    var hours = parseInt(curShowTimeSeconds/3600);
-    var minutes = parseInt((curShowTimeSeconds - hours*3600)/60);
-    var seconds = curShowTimeSeconds % 60;
-
-    renderDigit(MARGIN_LEFT,MARGIN_TOP,parseInt(hours/10),cxt);
-    renderDigit(MARGIN_LEFT+15*(RADIUS+1),MARGIN_TOP,parseInt(hours%10),cxt);
-    renderDigit(MARGIN_LEFT+30*(RADIUS+1),MARGIN_TOP,10,cxt);
-    renderDigit(MARGIN_LEFT+39*(RADIUS+1),MARGIN_TOP,parseInt(minutes/10),cxt);
-    renderDigit(MARGIN_LEFT+54*(RADIUS+1),MARGIN_TOP,parseInt(minutes%10),cxt);
-    renderDigit(MARGIN_LEFT+69*(RADIUS+1),MARGIN_TOP,10,cxt);
-    renderDigit(MARGIN_LEFT+78*(RADIUS+1),MARGIN_TOP,parseInt(seconds/10),cxt);
-    renderDigit(MARGIN_LEFT+93*(RADIUS+1),MARGIN_TOP,parseInt(seconds%10),cxt);
 }
 
 /**
- * Draw digit
- * @param x
- * @param y
- * @param num
+ * 跟新时间，判断时间的变化，添加小球
+ */
+function updateTime() {
+    var nextShowTimeSeconds = getCurShowTimeSeconds();
+    var nextHours = parseInt(nextShowTimeSeconds / 3600);
+    var nextMinutes = parseInt((nextShowTimeSeconds - nextHours * 3600) / 60);
+    var nextSeconds = nextShowTimeSeconds % 60;
+
+    var curHours = parseInt(curShowTimeSeconds / 3600);
+    var curMinutes = parseInt((curShowTimeSeconds - curHours * 3600) / 60);
+    var curSeconds = curShowTimeSeconds % 60;
+
+    if (nextSeconds != curSeconds) {
+        if (parseInt(nextHours / 10) != parseInt(curHours / 10)) {
+            addBalls(MARGIN_LEFT, MARGIN_TOP, parseInt(nextHours / 10));
+        }
+
+        if (parseInt(nextHours % 10) != parseInt(curHours % 10)) {
+            addBalls(MARGIN_LEFT + 15 * (RADIUS + 1), MARGIN_TOP, parseInt(nextHours % 10));
+        }
+
+        if (parseInt(nextMinutes / 10) != parseInt(curMinutes / 10)) {
+            addBalls(MARGIN_LEFT + 39 * (RADIUS + 1), MARGIN_TOP, parseInt(curMinutes / 10));
+        }
+
+        if (parseInt(nextMinutes % 10) != parseInt(curMinutes % 10)) {
+            addBalls(MARGIN_LEFT + 54 * (RADIUS + 1), MARGIN_TOP, parseInt(curMinutes % 10));
+        }
+
+        if (parseInt(nextSeconds / 10) != parseInt(curSeconds / 10)) {
+            addBalls(MARGIN_LEFT + 78 * (RADIUS + 1), MARGIN_TOP, parseInt(nextSeconds / 10));
+        }
+
+        if (parseInt(nextSeconds % 10) != parseInt(curSeconds % 10)) {
+            addBalls(MARGIN_LEFT + 93 * (RADIUS + 1), MARGIN_TOP, parseInt(nextSeconds % 10));
+        }
+
+    }
+
+    updateBalls();
+
+    if (nextShowTimeSeconds != curShowTimeSeconds) {
+        curShowTimeSeconds = nextShowTimeSeconds;
+    }
+}
+
+/**
+ * 跟新小球的运动状态
+ */
+function updateBalls() {
+    for (var i = 0; i < balls.length; i++) {
+        balls[i].x += balls[i].vx;
+        balls[i].y += balls[i].vy;
+        balls[i].vy += balls[i].g;
+
+        if (balls[i].y + RADIUS >= WINDOW_HEIGHT) {
+            balls[i].y = WINDOW_HEIGHT - RADIUS;
+            balls[i].vy = -balls[i].vy * 0.75;
+        }
+    }
+
+    var cnt = 0;
+    for (var i = 0; i < balls.length; i++) {
+
+        if (balls[i].x + RADIUS > 0 && balls[i].x - RADIUS < WINDOW_WIDTH) {
+            balls[cnt++] = balls[i];
+        }
+    }
+
+    while(balls.length > cnt){
+        balls.pop();
+    }
+}
+
+/**
+ * 随机添加小球
+ * @param x 小球的x坐标
+ * @param y 小球的y坐标
+ * @param num 小球表示的数字
+ */
+function addBalls(x, y, num) {
+
+    for (var i = 0; i < digit[num].length; i++) {
+        for (var j = 0; j < digit[num][i].length; j++) {
+            if (digit[num][i][j] == 1) {
+                var aBall = {
+                    x: x + j * 2 * (RADIUS + 1) + (RADIUS + 1),
+                    y: y + i * 2 * (RADIUS + 1) + (RADIUS + 1),
+                    g: 1.5 + Math.random(),
+                    vx: Math.pow(-1, Math.ceil(Math.random() * 1000)) * 4,
+                    vy: -5,
+                    color: colors[Math.floor(Math.random() * colors.length)]
+                }
+                balls.push(aBall);
+            }
+        }
+    }
+}
+
+/**
+ * 获取当前时间
+ * @returns {number}
+ */
+function getCurShowTimeSeconds() {
+    var curTime = new Date();
+    var ret = endTime.getTime() - curTime.getTime();
+    ret = Math.round(ret / 1000);
+
+    return ret >= 0 ? ret : 0;
+}
+
+/**
+ * 渲染生成倒计时的状态
  * @param cxt
  */
-function renderDigit(x,y,num,cxt){
+function render(cxt) {
+
+    cxt.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    var hours = parseInt(curShowTimeSeconds / 3600);
+    var minutes = parseInt((curShowTimeSeconds - hours * 3600) / 60);
+    var seconds = curShowTimeSeconds % 60;
+
+    renderDigit(MARGIN_LEFT, MARGIN_TOP, parseInt(hours / 10), cxt);
+    renderDigit(MARGIN_LEFT + 15 * (RADIUS + 1), MARGIN_TOP, parseInt(hours % 10), cxt);
+    renderDigit(MARGIN_LEFT + 30 * (RADIUS + 1), MARGIN_TOP, 10, cxt);
+    renderDigit(MARGIN_LEFT + 39 * (RADIUS + 1), MARGIN_TOP, parseInt(minutes / 10), cxt);
+    renderDigit(MARGIN_LEFT + 54 * (RADIUS + 1), MARGIN_TOP, parseInt(minutes % 10), cxt);
+    renderDigit(MARGIN_LEFT + 69 * (RADIUS + 1), MARGIN_TOP, 10, cxt);
+    renderDigit(MARGIN_LEFT + 78 * (RADIUS + 1), MARGIN_TOP, parseInt(seconds / 10), cxt);
+    renderDigit(MARGIN_LEFT + 93 * (RADIUS + 1), MARGIN_TOP, parseInt(seconds % 10), cxt);
+
+    //动态生成小球
+    for (var i = 0; i < balls.length; i++) {
+        cxt.fillStyle = balls[i].color;
+
+        cxt.beginPath();
+        cxt.arc(balls[i].x, balls[i].y, RADIUS, 0, 2 * Math.PI, true);
+        cxt.closePath();
+        cxt.fill();
+    }
+}
+
+/**
+ * 绘制数字
+ * @param x 小球的x坐标
+ * @param y 小球的y坐标
+ * @param num 小球的数字
+ * @param cxt 画布内容对象
+ */
+function renderDigit(x, y, num, cxt) {
 
     cxt.fillStyle = "rgb(0,102,153)";
 
-    for(var i = 0; i < digit[num].length;i++){
+    for (var i = 0; i < digit[num].length; i++) {
 
-        for(var j = 0; j <digit[num][i].length;j++ ){
+        for (var j = 0; j < digit[num][i].length; j++) {
 
-            if (digit[num][i][j] == 1){
+            if (digit[num][i][j] == 1) {
                 cxt.beginPath();
-                cxt.arc(x+j*2*(RADIUS+1)+(RADIUS+1),y+i*2*(RADIUS+1)+(RADIUS+1),RADIUS,0,Math.PI*2);
+                cxt.arc(x + j * 2 * (RADIUS + 1) + (RADIUS + 1), y + i * 2 * (RADIUS + 1) + (RADIUS + 1), RADIUS, 0, Math.PI * 2);
                 cxt.closePath();
                 cxt.fill();
             }
